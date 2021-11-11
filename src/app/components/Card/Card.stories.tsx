1077d41a2eb97c7c41f89013f339aa2fd79ca3bb
Jan <janhendrikstruve@gmail.com>
2021-11-11 11:06:28 +0100

    Split cards into seperate cards with more or less detail

--- src/app/components/Card/Card.stories.tsx
+++ src/app/components/Card/Card.stories.tsx
@@ -10,30 +10,5 @@ export const standard = (): JSX.Element => (
   <Card
     title={'I am a freaking Card'}
     description={'Do not mess around with me man'}
-    cardTags={['#coolest', '#card', '#in', '#town']}
-  ></Card>
-);
-
-export const lessTags = (): JSX.Element => (
-  <Card
-    title={'I am a freaking Card'}
-    description={'Do not mess around with me man'}
-    cardTags={['#coolest']}
-  ></Card>
-);
-
-export const moreTags = (): JSX.Element => (
-  <Card
-    title={'I am a freaking Card'}
-    description={'Do not mess around with me man'}
-    cardTags={['#coolest', '#card', '#in', '#town', '#you', '#little', 'kek']}
-  ></Card>
-);
-
-export const noTags = (): JSX.Element => (
-  <Card
-    title={'I am a freaking Card'}
-    description={'Do not mess around with me man'}
-    cardTags={[]}
   ></Card>
 );
--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,28 +1,18 @@
 import React from 'react';
-import Tag from '../Tag/Tag';
 import styled from 'styled-components';
-import type { ReactNode } from 'react';
+import Button from '../Button/Button';
 
 type cardProps = {
   title: string;
   description: string;
-  cardTags?: ReactNode[];
 };
 
-export default function Card({
-  title,
-  description,
-  cardTags,
-}: cardProps): JSX.Element {
+export default function Card({ title, description }: cardProps): JSX.Element {
   return (
     <StyledCard>
       <h2>{title}</h2>
       <p>{description}</p>
-      <StyledList>
-        {cardTags?.map((tag) => (
-          <Tag children={tag} />
-        ))}
-      </StyledList>
+      <Button>more details</Button>
     </StyledCard>
   );
 }
@@ -34,11 +24,4 @@ const StyledCard = styled.section`
   border-radius: 8px;
   background-color: lightblue;
   justify-items: center;
-`;
-
-const StyledList = styled.ul`
-  display: flex;
-  justify-content: space-evenly;
-  padding: 0;
-  flex-wrap: wrap;
 `;
